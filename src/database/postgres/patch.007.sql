da5891952e45f198fcd1b981d51a5e0e2cbb006e
Petr Bravenec <petr@bravenec.org>
2025-07-01 13:52:27 +0200

    Upravený patch007

--- src/database/postgres/patch.007.sql
+++ src/database/postgres/patch.007.sql
@@ -5,6 +5,7 @@ insert into version (version) values (7);
 drop view if exists attendance.events_view;
 
 create view attendance.events_view as
+
 select
         e.event,
         e.date,
@@ -15,12 +16,39 @@ select
         p.surname,
         e.valid,
         e.user_edited,
-        u.name as user_edited_name
-        
+        u.name as user_edited_name,
+        case when e.valid and     t.end_state and     prev.end_state then 'Unexpected END'  else 
+        case when e.valid and not t.end_state and not prev.end_state then 'Missing END'     else 
+            null
+            end end as error
+
     from attendance.events e
     left join attendance.employees   p using (employee)
     left join users                  u on (e.user_edited = u."user")
     left join attendance.event_types t using (event_type)
+
+    left join lateral (select * 
+            from attendance.events ep
+            left join attendance.event_types t using (event_type)
+            where ep.valid
+              and ep.date < e.date
+              and ep.employee = e.employee
+              and not t.passage
+            order by date desc
+            limit 1
+            ) prev on true
+
+    left join lateral (select * 
+            from attendance.events ep
+            left join attendance.event_types t using (event_type)
+            where ep.valid
+              and ep.date > e.date
+              and ep.employee = e.employee
+              and not t.passage
+            order by date 
+            limit 1
+            ) next on true
+
     ;
 
 commit;
